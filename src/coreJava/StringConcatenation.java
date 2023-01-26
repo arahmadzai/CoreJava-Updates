37c602250f1e6c1248a9fa33daefe30128f0c325
Abidullah Rahimi <abid_rahimi@hotmail.com>
2023-01-26 01:23:39 -0500

    StringConcatenation class created

---
+++ src/coreJava/StringConcatenation.java
@@ -0,0 +1,15 @@
+package coreJava;
+
+public class StringConcatenation {
+
+    public static void main(String[] args){
+
+        System.out.println("This is " + "one string.");
+
+        int age = 35;
+        System.out.println("I am " + age + " years old.");
+
+        String language = "Java";
+        System.out.println("My favorite programming language is " + language + ".");
+    }
+}
