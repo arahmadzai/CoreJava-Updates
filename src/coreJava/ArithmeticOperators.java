a64213e269c905641051c5c2abb9fbcf8f240681
Abidullah Rahimi <abid_rahimi@hotmail.com>
2023-01-26 01:40:20 -0500

    ArithmeticOperators class created

---
+++ src/coreJava/ArithmeticOperators.java
@@ -0,0 +1,50 @@
+package coreJava;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args){
+
+        //Addition:
+        System.out.println(1000 + 2000);
+        System.out.println(2.5 + 3.5);
+
+        System.out.println("-----------------------------------------------");
+
+        //Subtraction:
+        System.out.println(100 - 30);
+        int a = 300 - 100;
+        System.out.println(a);
+
+        System.out.println("-----------------------------------------------");
+
+        //multiplication
+        System.out.println( 20 * 6);
+        System.out.println(5.5 * 7);
+
+        System.out.println("-----------------------------------------------");
+
+        //Division:
+        System.out.println(10/4); //2
+        System.out.println(10.0 / 4); //2.5
+        System.out.println(10.0 / 4.0); //2.5
+        System.out.println(10 / 4.0); //2.5
+        System.out.println(10 / 4d); //2.5
+        System.out.println(10d / 4d); //2.5
+        System.out.println(10d / 4); //2.5
+        System.out.println(10f / 4); //2.5
+
+        System.out.println(20 / 6); //3
+        System.out.println(20d / 6); //3.3333333333333335
+
+        System.out.println("-----------------------------------------------");
+
+        //Remainder:
+        System.out.println(10%4); //2
+
+        System.out.println(20 % 5); //0
+
+
+
+
+    }
+}
