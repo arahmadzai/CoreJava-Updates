dab3b09e9cbd4e095ab8397323032fc648a471a8
Abidullah Rahimi <abid_rahimi@hotmail.com>
2023-01-27 00:57:33 -0500

    MainMethod class created

---
+++ src/coreJava/methods/MainMethod.java
@@ -0,0 +1,21 @@
+package coreJava.methods;
+
+public class MainMethod {
+
+    public static void main(String[] args){// in order to run our program main method is must
+        //public: it must be accessible to outside the class
+        //static: it can be called without an object
+        //void: it doesn't return anything, it only runs and ends.
+        //String[] args: is a part of method syntax, it will store all the command line arguments.
+        // when we pass arguments in command line String[] args is used,
+        // if we don't use command line String[] args has no purpose in our code.
+
+        System.out.println("Hello Afghanistan");
+        System.err.println("Hello Afghanistan");
+
+
+
+    }
+
+
+}
